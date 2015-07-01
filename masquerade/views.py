94b448be8d20456481fc54a2a56c7aed204d5e87
Calvin DeBoer <cdeboer@horizoninvestments.com>
2015-07-01 11:24:58 -0400

    Added ability to restrict masqueraders to users who share a common auth Group.

--- masquerade/views.py
+++ masquerade/views.py
@@ -11,17 +11,31 @@ MASQUERADE_REDIRECT_URL = getattr(settings, 'MASQUERADE_REDIRECT_URL', '/')
 MASQUERADE_REQUIRE_SUPERUSER = getattr(settings,
   'MASQUERADE_REQUIRE_SUPERUSER', False)
 
+MASQUERADE_REQUIRE_COMMON_GROUP = getattr(settings,
+  'MASQUERADE_REQUIRE_COMMON_GROUP', False)
+
 def mask(request, template_name='masquerade/mask_form.html'):
     if not request.user.is_masked and not request.user.is_staff:
         return HttpResponseForbidden()
     elif not request.user.is_superuser and MASQUERADE_REQUIRE_SUPERUSER:
-        return HttpResponseForbidden()
+        return HttpResponseForbidden()    
 
     if request.method == 'POST':
         form = MaskForm(request.POST)
         if form.is_valid():
+            
+            mask_user = form.cleaned_data['mask_user']
+            
+            if MASQUERADE_REQUIRE_COMMON_GROUP:
+                
+                user_groups = request.user.groups.all()
+                mask_groups = mask_user.groups.all()
+                
+                if not any(x in mask_groups for x in user_groups):
+                    return HttpResponseForbidden()
+            
             # turn on masquerading
-            request.session['mask_user'] = form.cleaned_data['mask_user']
+            request.session['mask_user'] = mask_user
             mask_on.send(sender=form,
                 mask_username=form.cleaned_data['mask_user'])
             return HttpResponseRedirect(MASQUERADE_REDIRECT_URL)
