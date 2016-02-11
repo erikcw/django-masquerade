3e838ac0a7a354a8f0aa910096bd541fba9bc563
Adam Duren <adamduren@gmail.com>
2016-02-11 13:51:16 -0500

    Commit left out files

---
+++ run_tests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+from importlib import import_module
+
+if __name__ == "__main__":
+    settings.configure(
+        LOGGING={},
+        LOGGING_CONFIG={},
+        INSTALLED_APPS=[
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'masquerade',
+        ],
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'test',
+            },
+        },
+        MIDDLEWARE_CLASSES = [
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+            'masquerade.middleware.MasqueradeMiddleware',
+        ],
+        ROOT_URLCONF='masquerade.test_urls',
+        DEBUG=False
+    )
+
+    django.setup()
+
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(['masquerade'])
+    sys.exit(bool(failures))
