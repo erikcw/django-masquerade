26a95b8e6f9786844be0cc990436917c73403276
Matthew T Rich <m-rich@northwestern.edu>
2016-02-12 09:56:11 -0600

    bumping version to 1.0 since no further functionality changes planned

--- masquerade/__init__.py
+++ masquerade/__init__.py
@@ -2,9 +2,9 @@
 django-masquerade
 """
 __version_info__ = {
-    'major': 0,
-    'minor': 2,
-    'micro': 1,
+    'major': 1,
+    'minor': 0,
+    'micro': 0,
     'releaselevel': 'final',
     'serial': 1
 }
