22dda778e95a03607217d488e5030ef7f449976f
Calvin Deboer <cdeboer@horizoninvestments.com>
2016-12-01 09:51:52 -0500

    updated masquerade.

--- masquerade/middleware.py
+++ masquerade/middleware.py
@@ -1,6 +1,13 @@
 from masquerade.users import UserModel
 
 class MasqueradeMiddleware(object):
+    def __init__(self, *args):
+        if args:
+            self.get_response = args[0]
+
+    def __call__(self, request):
+        self.process_request(request)
+
     def process_request(self, request):
         """Checks for the presence of "mask_user" in the session. The value
         should be the username of the user to be masqueraded as. Note we
@@ -15,30 +22,9 @@ class MasqueradeMiddleware(object):
             User = UserModel()
             try:
                 original_user = request.user
-                request.user = \
-                  User.objects.get_by_natural_key(request.session['mask_user'])
-                request.user.is_masked = True
-                request.user.original_user = original_user
-            except User.DoesNotExist:
-                pass
-
-def masquerade_middleware(get_response):
-    def middleware(request):
-        """Checks for the presence of "mask_user" in the session. The value
-        should be the username of the user to be masqueraded as. Note we
-        also set the "is_masked" attribute to true in that case so that when we
-        hit the "mask" or "unmask" URLs this user is properly recognized,
-        rather than the user they're masquerading as :) """
-
-        request.user.is_masked = False
-        request.user.original_user = None
-
-        if 'mask_user' in request.session:
-            User = UserModel()
-            try:
-                original_user = request.user
                 request.user = User.objects.get_by_natural_key(
-                                    request.session['mask_user'])
+                    request.session['mask_user']
+                )
                 request.user.is_masked = True
                 request.user.original_user = original_user
             except User.DoesNotExist:
