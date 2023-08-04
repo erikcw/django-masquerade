170fdef60157f11481446b39963e91a1a8d2d1b1
Erik Wickstrom <erik@erikwickstrom.com>
2023-08-04 10:33:31 -0700

    Update import path to re_path for django 4.2

--- masquerade/urls.py
+++ masquerade/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import re_path
+from django.urls import re_path
 
 from masquerade.views import mask, unmask
 
 urlpatterns = [
-    re_path(r'^mask/$', mask, name='masquerade-mask'),
-    re_path(r'^unmask/$', unmask, name='masquerade-unmask'),
+    re_path(r"^mask/$", mask, name="masquerade-mask"),
+    re_path(r"^unmask/$", unmask, name="masquerade-unmask"),
 ]
